8dcc7133bd264f232d589acb83129c3c43932a4b
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-10-17 10:21:42 -0300

    feat: bottomNavigation

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -4,6 +4,7 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:green_grocer/src/auth/components/custom_text_field.dart';
 import 'package:green_grocer/src/auth/sign_up_screen.dart';
+import 'package:green_grocer/src/base/base_screen.dart';
 import 'package:green_grocer/src/config/custom_colors.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -107,7 +108,15 @@ class SignInScreen extends StatelessWidget {
                             ),
                           ),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (c) {
+                                return const BaseScreen();
+                              },
+                            ),
+                          );
+                        },
                         child: const Text(
                           'Entrar',
                           style: TextStyle(
