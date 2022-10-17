e28592e11b80cb8aa3aaca1e263d61607e56543f
Anderson-Andre-P <andreandersoncaue.e@gmail.com>
2022-10-17 09:38:50 -0300

    feat: navegacao sign up screen

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -14,74 +14,93 @@ class SignUpScreen extends StatelessWidget {
         child: SizedBox(
           height: size.height,
           width: size.width,
-          child: Column(
+          child: Stack(
             children: [
-              const Expanded(
-                child: Center(
-                  child: Text(
-                    'Cadastro',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35.0,
-                    ),
-                  ),
-                ),
-              ),
-              //Formulário
-              Container(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 32,
-                  vertical: 40,
-                ),
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.vertical(
-                    top: Radius.circular(45),
-                  ),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    const CustomTextField(
-                      icon: Icons.email,
-                      label: 'Email',
-                    ),
-                    const CustomTextField(
-                      icon: Icons.lock,
-                      label: 'Senha',
-                      isSecret: true,
-                    ),
-                    const CustomTextField(
-                      icon: Icons.person,
-                      label: 'Nome',
-                    ),
-                    const CustomTextField(
-                      icon: Icons.phone,
-                      label: 'Celular',
-                    ),
-                    const CustomTextField(
-                      icon: Icons.file_copy,
-                      label: 'CPF',
-                    ),
-                    SizedBox(
-                      height: 50.0,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: const Text(
-                          "Cadastrar usuário",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            color: Colors.white,
-                          ),
+              Column(
+                children: [
+                  const Expanded(
+                    child: Center(
+                      child: Text(
+                        'Cadastro',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35.0,
                         ),
                       ),
-                    )
-                  ],
+                    ),
+                  ),
+                  //Formulário
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 32,
+                      vertical: 40,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(45),
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const CustomTextField(
+                          icon: Icons.email,
+                          label: 'Email',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.lock,
+                          label: 'Senha',
+                          isSecret: true,
+                        ),
+                        const CustomTextField(
+                          icon: Icons.person,
+                          label: 'Nome',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.phone,
+                          label: 'Celular',
+                        ),
+                        const CustomTextField(
+                          icon: Icons.file_copy,
+                          label: 'CPF',
+                        ),
+                        SizedBox(
+                          height: 50.0,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              "Cadastrar usuário",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                left: 10,
+                top: 10,
+                child: SafeArea(
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.white,
+                    ),
+                  ),
                 ),
               ),
             ],
